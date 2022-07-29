e21e096c96784ebf713f01bd84963eaa0e86d820
davidMurillo86 <david.murillo86@gmail.com>
2022-07-29 15:05:07 -0600

    moved files

--- cypress/e2e/signUp.cy.ts
+++ cypress/e2e/signUp.cy.ts
@@ -1,6 +1,6 @@
-import IndexPage from '../../pages/IndexPage'
-import SignUpPage from '../../pages/SignUpPage'
-import LoginPage from '../../pages/LoginPage'
+import IndexPage from '../pages/IndexPage'
+import SignUpPage from '../pages/SignUpPage'
+import LoginPage from '../pages/LoginPage'
 
 describe('Sign up tests', () => {
   it('Sign up new user', () => {
